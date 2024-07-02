168c614256b5c9cf28c65b485ff44ffbcfa477dc
jdrli <jackyli9875@gmail.com>
2024-07-01 21:47:22 -0400

    Finished 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR';
+    let output = '';
+    output = output.padEnd((string.length) * num, string);
+    return output;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
