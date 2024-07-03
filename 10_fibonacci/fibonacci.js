bb6677a13a80b9ab1210850479e58cbb7b1b54bd
jdrli <jackyli9875@gmail.com>
2024-07-03 19:45:46 -0400

    Finished 10_fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(nthNumber) {
+    nthNumber = parseInt(nthNumber);
+    let fibArr = [1, 1]
+    let i = 2;
+    if (nthNumber < 0) {
+        return "OOPS";
+    }
+    else if (nthNumber == 0) {
+        return 0;
+    }
+    else {
+        while (i < nthNumber) {
+            fibArr.push(fibArr[i - 1] + fibArr[i - 2]);
+            i++;
+        }
+        return fibArr[nthNumber - 1];
+    }
 };
 
 // Do not edit below this line
