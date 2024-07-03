a7010cc237fc7d1a2035899290b9ee849acceab4
jdrli <jackyli9875@gmail.com>
2024-07-03 19:34:40 -0400

    Finished 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,50 @@
-const add = function() {
-	
-};
+const add = function (num1, num2) {
+  return num1 + num2;
+}
 
-const subtract = function() {
-	
-};
+const subtract = function (num1, num2) {
+  return num1 - num2;
+}
 
-const sum = function() {
-	
-};
+const sum = function (array) {
+  let output = 0;
 
-const multiply = function() {
+  for (let i = 0; i < array.length; i++) {
+    output += array[i];
+  }
 
-};
+  return output;
+}
 
-const power = function() {
-	
-};
+const multiply = function (array) {
+  let output = 1;
 
-const factorial = function() {
-	
-};
+  for (const num of array) {
+    output *= num;
+  }
+  
+  return output; 
+}
+
+const power = function (base, exponent) {
+  let output = 1;
+
+  for (let i = 0; i < exponent; i++) {
+    output *= base;
+  }
+
+  return output;
+}
+
+const factorial = function (number) {
+  let output = 1;
+
+  for (let i = number; i > 0; i--){
+    output *= i;
+  }
+
+  return output
+}
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +53,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
