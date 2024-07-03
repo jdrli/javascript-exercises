470a54b3a91225b46b660dfbda33247baf0e40ed
jdrli <jackyli9875@gmail.com>
2024-07-03 14:10:58 -0400

    Finished 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+    const sumAll = function(first, second) {
+        let output = 0;
+        
+        if (first !== parseInt(first) || second !== parseInt(second) || first < 0 || second < 0) {
+            return 'ERROR';
+        }
 
+        if (second > first){
+            for (let i = first; i <= second; i++) {
+                output += i;
+            }
+        }
+        else if (first > second){
+            for (let i = second; i <= first; i++) {
+                output += i;
+            }
+        }
+
+        return output;
 };
 
 // Do not edit below this line
